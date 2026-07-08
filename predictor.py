88c6a6350519b3c9202fb6b410d8d09f666b9528
Vikash arvind <vikasharvind05@gmail.com>
2026-07-08 12:03:59 +0530

    Refactor predict function and clean imports

--- predictor.py
+++ predictor.py
@@ -1,21 +1,16 @@
 import joblib
 import pandas as pd
-from pyexpat import model
-
-from train import pipeline
 
 pipeline=joblib.load("model/pipeline.pkl")
 
-
 def predict(data: dict):
-    df= pd.DataFrame([data])
-    prediction = pipeline.predict(df)[0])
-    probability = pipeline.predict_proba(df)[0].tolist()
+    df=pd.DataFrame([data])
+    prediction=pipeline.predict(df)[0]
+    probability=pipeline.predict_proba(df)[0].tolist()
     return{
-      "prediction":int(prediction),
-        "probability": {
-            "No Survival": probability[0],
-            "Survival": probability[1]
+        "prediction":int(prediction),
+        "probability":{
+            "No Survival":probability[0],
+            "Survival":probability[1]
         }
     }
-
